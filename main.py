486a081fc4a3f79319d0eb06a9833dfd43a18f04
tomerson <tomerson@bgu.ac.il>
2023-03-10 03:19:06 +0200

    basic folder structure with getattr override working correctly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print ("hello")
-print("yes1")
-print("yes2")
-print("yes23")
-print("yes22")
+import os
+from Directory import Directory
 
+#print(os.path.abspath("."))
+
+x = Directory("x")
+y = Directory("y",x)
+z = Directory("z",y)
+print(x.y.z.printPath())
+print("finish")
