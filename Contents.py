d0505e14ee0ce01412a5260cc7b1d59a074ffae0
tomerson <tomerson@bgu.ac.il>
2023-03-11 01:07:55 +0200

    New module for folder and file, with correct getattr method

--- Contents.py
+++ Contents.py
@@ -23,6 +23,11 @@ class Directory:
             if self._father_folder != None:
                 self._father_folder.addSubfolder(self)
 
+    def addFile(self,file):
+            if file not in self._contents:
+                self._contents.append(file)
+
+
     def __getattr__(self, attr):
         try:
             for folder in self._contents:
@@ -32,6 +37,49 @@ class Directory:
         except AttributeError as e:
             raise e
 
-#C:\Users\tomer\PycharmProjects\Training\x\y\z
-#1.enter y as a content of x
-#2.create the path as abspath\x
+    def __str__(self):
+        return f"Folder {self._name} in {self.printPath()}"
+    def __repr__(self):
+        return f"Folder {self._name} in {self.printPath()}"
+
+    def getContents(self):
+        return self._contents
+
+class File:
+
+    def __init__(self,name,content="",father_folder=None):
+        self._name=name
+        self._content=content
+        self._father_folder=father_folder
+        self._path = self.printPath()
+        if self._father_folder != None:
+            self._father_folder.addFile(self)
+
+    def printPath(self):
+        if self._father_folder == None:
+            return os.path.abspath('.')
+        else:
+            return f"{self._father_folder.printPath()}\{self._father_folder._name}"
+
+    def printContent(self):
+        print(self._content)
+
+    def getContent(self):
+        return self._content
+
+    def __str__(self):
+        return f"File {self._name} in {self.printPath()}"
+
+    def __repr__(self):
+        return f"File {self._name} in {self.printPath()}"
+
+def filesThatContains(folder:Directory, word:str):
+    files=[]
+    for item in folder.getContents():
+        if isinstance(item,File):
+            if word in item.getContent():
+                files.append(item)
+        elif isinstance(item,Directory):
+            files.extend(filesThatContains(item,word))
+    return files
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from Contents import Directory
+from Contents import File
+from Contents import filesThatContains
 
 #print(os.path.abspath("."))
 
@@ -14,9 +16,19 @@ from Contents import Directory
 #print(os.path.abspath('.'))
 
 x = Directory("x")
-y= Directory("y34",x)
-z= Directory("z56",y)
-print(x.printPath())
-print(y.printPath())
-print(z.printPath())
-print(x.y34.z56.printPath())
+x.addSubfolder(Directory("y",x))
+x.y.addSubfolder(Directory("z",x.y))
+x.y.z.addFile(File("someFile","hello world",x.y.z))
+x.y.addFile(File("someFile2","hello",x.y))
+#y= Directory("y34",x)
+#z= Directory("z56",y)
+#print(x.printPath())
+#print(y.printPath())
+#print(z.printPath())
+#print(x.y34.z56.printPath())
+print(x.y.z.printPath())
+print(x.y.z.someFile.printPath())
+x.y.z.someFile.printContent()
+
+print(filesThatContains(x,"hello"))
+
