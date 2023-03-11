d589935ef4d2a781e2c626848716c3c89d86037a
tomerson <tomerson@bgu.ac.il>
2023-03-11 18:08:17 +0200

    Fixed based on review from 11.3

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from Directory import Directory
 from File import File
 
-def filesThatContains(folder:'Directory', word:str):
+def files_that_contains(folder: 'Directory', word:str):
     files=[]
     for item in folder.contents:
         if isinstance(item,File):
             if word in item.content:
                 files.append(item)
         elif isinstance(item,Directory):
-            files.extend(filesThatContains(item,word))
+            files.extend(files_that_contains(item, word))
     return files
 
 
@@ -18,19 +18,12 @@ if __name__ == "__main__":
     x.y.add_subfolder(Directory("z", x.y))
     x.y.z.add_file(File("someFile", "hello world", x.y.z))
     x.y.add_file(File("someFile2", "hello", x.y))
-    #y= Directory("y34",x)
-    #z= Directory("z56",y)
-    #print(x.printPath())
-    #print(y.printPath())
-    #print(z.printPath())
-    #print(x.y34.z56.printPath())
     print(x.y.z.print_path())
     print(x.y.z.someFile.print_path())
     x.y.z.someFile.print_content()
 
-    print(filesThatContains(x,"hello"))
-    print(filesThatContains(x,"world"))
-    print(filesThatContains(x,"abc"))
-    print(filesThatContains(x.y,"world"))
-    #x.y.z.someFile.addFile(x.y.someFile2)
+    print(files_that_contains(x, "hello"))
+    print(files_that_contains(x, "world"))
+    print(files_that_contains(x, "abc"))
+    print(files_that_contains(x.y, "world"))
 
