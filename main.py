2778ac0feda8a2444177041154db04ceff953871
tomerson <tomerson@bgu.ac.il>
2023-03-15 11:59:56 +0200

    changed main functionality

--- main.py
+++ main.py
@@ -13,17 +13,10 @@ def files_that_contains(folder: 'Directory', word:str):
 
 
 if __name__ == "__main__":
-    x = Directory("x")
-    x.add_subfolder(Directory("y", x))
-    x.y.add_subfolder(Directory("z", x.y))
-    x.y.z.add_file(File("someFile", "hello world", x.y.z))
-    x.y.add_file(File("someFile2", "hello", x.y))
-    print(x.y.z.print_path())
-    print(x.y.z.someFile.print_path())
-    x.y.z.someFile.print_content()
+    # files_scanner = Search()
+    # result = files_scanner.search_by_content('/path/to/root/dir', 'the-word-to-look-for')
 
-    print(files_that_contains(x, "hello"))
-    print(files_that_contains(x, "world"))
-    print(files_that_contains(x, "abc"))
-    print(files_that_contains(x.y, "world"))
+    # result should include list of all paths that has the searched word..
+
+
 
