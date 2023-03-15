d48f9ff94991dfe2045c7fdc003af75b4a50c0cb
Saar <saarbu@post.bgu.ac.il>
2023-03-15 12:05:38 +0200

    Added function in main to support the required function call for the search script

--- Directory.py
+++ Directory.py
@@ -1,38 +1,61 @@
 import os
 import ntpath
 
+
 class FailedToCreateDirectory(Exception):
     pass
+
+
 class FailedToReadFromFile(Exception):
     pass
 
+
 class Directory(object):
-    def __init__(self,path:str,father_directory:'Directory'=None):
+    def __init__(self, path:str, father_directory:'Directory'= None):
+        if not os.path.isdir(path):
+            raise FailedToCreateDirectory("path is not a directory")
         self._path = path
         self._father_directory = father_directory
         self._subdirectories = []
         self._files = []
-        self.scan_folder()
+        self.__scan_folder()
 
-    def scan_folder(self):
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def father_directory(self):
+        return self._father_directory
+
+    @property
+    def subdirectories(self):
+        return self._subdirectories
+
+    @property
+    def files(self):
+        return self._files
+
+    def __scan_folder(self):
         for item in os.listdir(self._path):
-           if os.path.isdir(f"{self._path}{item}"):
-               try:
-                x = Directory(f"{self._path}{item}/",self)
-                self._subdirectories.append(x)
-               except:
-                  raise FailedToCreateDirectory(f"{self._path}{item}/")
-           else:
+            if os.path.isdir(f"{self._path}{item}"):
+                try:
+                    x = Directory(f"{self._path}{item}/",self)
+                    self._subdirectories.append(x)
+                except:
+                    raise FailedToCreateDirectory(f"{self._path}{item}/")
+            else:
                self._files.append(f"{self._path}{item}")
 
     def lookup_str_in_files(self,word:str):
+        if not os.path.isdir(self._path):
+            raise FailedToCreateDirectory("path is no longer a directory")
         res = []
         for file in self._files:
             try:
                 f = open(file,"r")
                 if word in f:
-                    path, file_name = ntpath.split(file)
-                    res.append(file_name)
+                    res.append(file)
             except:
                 raise FailedToReadFromFile(f"{self._path}{file}")
         for dir in self._subdirectories:
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 import Directory
 
+
+def files_that_contains_word_in_dir(dir_path:str, word:str):
+    res = []
+    directory = Directory.Directory(dir_path)
+    return directory.lookup_str_in_files(word)
+
+
 if __name__ == '__main__':
     #Probably have to adapt the paths to Linux to make it work there.
-    dir1 = Directory.Directory("D:/root_test/")
-    print(dir1.lookup_str_in_files("Hello World"))
+    print(files_that_contains_word_in_dir("C:/test_root/","Hello World"))
 
