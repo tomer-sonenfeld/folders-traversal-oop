79780521cb7994c4d9ea7096219b610394113d64
tomerson <tomerson@bgu.ac.il>
2023-03-15 15:20:57 +0200

    fixed issues of last merge

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     files_scanner = Search()
     result= files_scanner.search_by_content(r"C:\Users\tomer\PycharmProjects\Training\testFolder","hello")
-    print(result)
+    print(result
 
 
 
