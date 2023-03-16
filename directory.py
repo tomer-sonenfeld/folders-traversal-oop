a5a5967e7a4fd9d1beaa4e5f5622706c50919336
tomerson <tomerson@bgu.ac.il>
2023-03-16 13:19:00 +0200

    changed item name in for loop of traverse

--- directory.py
+++ directory.py
@@ -4,25 +4,25 @@ from file import File
 class Directory:
     def __init__(self, path:str):
         self._path=path
-        self._my_files=[]
+        self._files_in_directory=[]
 
     def traverse(self):
         items_found=[]
-        for item in os.listdir(self._path):
-            if os.path.isfile(os.path.join(self._path,item)):
-                file_checked = File(os.path.join(self._path,item))
+        for inner_path in os.listdir(self._path):
+            if os.path.isfile(os.path.join(self._path,inner_path)):
+                file_checked = File(os.path.join(self._path,inner_path))
                 #self._my_files.append(file_checked)
                 items_found.append(file_checked)
             else:
-                dir = Directory(os.path.join(self._path,item))
+                dir = Directory(os.path.join(self._path,inner_path))
                 items_found.extend(dir.traverse())
-        self._my_files.extend(items_found)
+        self._files_in_directory.extend(items_found)
         return items_found
 
 
     def files_with_content(self,word:str):
         files_with_word=[]
-        for file in self._my_files:
+        for file in self._files_in_directory:
                 if file.include_word(word):
                     files_with_word.append(file.my_path())
 
@@ -30,7 +30,7 @@ class Directory:
 
     @property
     def my_files(self):
-        return self._my_files
+        return self._files_in_directory
 
     @property
     def path(self):
