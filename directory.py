0c86bd64a9ed0c0e7bbd57c1c91e622540ddcb4c
tomerson <tomerson@bgu.ac.il>
2023-03-17 00:59:34 +0200

    renamed items_found to files_found

--- directory.py
+++ directory.py
@@ -8,16 +8,16 @@ class Directory:
         self._path=path
 
     def _traverse(self) -> list:
-        items_found=[]
+        files_found=[]
         for inner_path in os.listdir(self._path):
             full_path=os.path.join(self._path,inner_path)
             if os.path.isfile(full_path):
                 _file = File(full_path)
-                items_found.append(_file)
+                files_found.append(_file)
             elif os.path.isdir(full_path):
                 child = Directory(full_path)
-                items_found.extend(child.traverse)
-        return items_found
+                files_found.extend(child.traverse)
+        return files_found
 
 
     def files_with_content(self,word:str) -> list:
