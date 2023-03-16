c03876add75b41fa7c4c57536abbccf79ffdbbc7
tomerson <tomerson@bgu.ac.il>
2023-03-16 12:14:08 +0200

    cleaned main function

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from Directory import Directory
-from File import File
-import os
 from Search import Search
-
-def files_that_contains(folder: 'Directory', word:str):
-    files=[]
-    for item in folder.contents:
-        if isinstance(item,File):
-            if word in item.content:
-                files.append(item)
-        elif isinstance(item,Directory):
-            files.extend(files_that_contains(item, word))
-    return files
-
-    def Search(self):
-        return Search()
-
 
 if __name__ == "__main__":
 
