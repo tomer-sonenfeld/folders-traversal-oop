d59e06c99875333e41f48c9e1b089a7e6f381e37
tomerson <tomerson@bgu.ac.il>
2023-03-16 12:50:19 +0200

    changed calls from property calls to attribute

--- directory.py
+++ directory.py
@@ -7,19 +7,19 @@ class Directory:
         self._my_files=[]
 
     def traverse(self):
-        for item in os.listdir(self.path):
-            if os.path.isfile(self.path+"\\"+item):
-                file_checked = File(self.path+"\\"+item)
-                self.my_files.append(file_checked)
+        for item in os.listdir(self._path):
+            if os.path.isfile(self._path+"\\"+item):
+                file_checked = File(self._path+"\\"+item)
+                self._my_files.append(file_checked)
             else:
-                dir = Directory(self.path+"\\"+item)
+                dir = Directory(self._path+"\\"+item)
                 dir.traverse()
-                self.my_files.extend(dir.my_files)
+                self.my_files.extend(dir._my_files)
 
 
     def files_with_content(self,word:str):
         files_with_word=[]
-        for file in self.my_files:
+        for file in self._my_files:
                 if file.include_word(word):
                     files_with_word.append(file.my_path())
 
