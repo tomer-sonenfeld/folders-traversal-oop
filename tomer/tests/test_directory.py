a124185e570f8d266796a9b090682188c956d05a
tomerson <tomerson@bgu.ac.il>
2023-03-19 19:40:54 +0200

    more complex traverse check

--- tomer/tests/test_directory.py
+++ tomer/tests/test_directory.py
@@ -5,10 +5,14 @@ def test_traverse():
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_folder")
     _dir = source.Directory(test_folder_path)
     files_found = _dir.traverse()
-    assert len(files_found) == 2
+    assert len(files_found) == 4
     expected_files = {os.path.join(test_folder_path,"test_file_hello"),
-           os.path.join(test_folder_path,"test_file_no_hello")}
+                      os.path.join(test_folder_path,"test_file_no_hello"),
+                      os.path.join(test_folder_path,"test_sub_folder","test_file_hello"),
+                      os.path.join(test_folder_path,"test_sub_folder","test_file_no_hello")
+                      }
     assert set([f.path for f in files_found]) == expected_files
+
 
 def test_files_with_content():
     word = "hello"
@@ -16,6 +20,7 @@ def test_files_with_content():
     _dir = source.Directory(test_folder_path)
     files_found = _dir.traverse()
     files_found_with_word = _dir.files_with_content(word)
-    assert len(files_found_with_word) == 1
-    expected_files = {os.path.join(test_folder_path, "test_file_hello")}
+    assert len(files_found_with_word) == 2
+    expected_files = {os.path.join(test_folder_path, "test_file_hello"),
+                      os.path.join(test_folder_path,"test_sub_folder","test_file_hello")}
     assert set([f.path for f in files_found_with_word]) == expected_files
