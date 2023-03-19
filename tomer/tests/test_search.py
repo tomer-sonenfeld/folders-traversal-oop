fc27b82ce9d2718b6a9cba324e803fcf46452d3e
tomerson <tomerson@bgu.ac.il>
2023-03-19 20:04:13 +0200

    fixture for search test

--- tomer/tests/test_search.py
+++ tomer/tests/test_search.py
@@ -2,10 +2,16 @@ import os
 from tomer import source
 import pytest
 
-def test_search_by_content_existing_folder():
-    files_scanner = source.search.Search()
+@pytest.fixture
+def word():
+    return "hello"
+
+@pytest.fixture
+def files_scanner():
+    return source.search.Search()
+
+def test_search_by_content_existing_folder(word,files_scanner):
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_folder")
-    word="hello"
     files_found = files_scanner.search_by_content(test_folder_path,word)
     expected_files = {os.path.join(test_folder_path, "test_file_hello"),
                       os.path.join(test_folder_path,"test_sub_folder","test_file_hello")
@@ -13,17 +19,13 @@ def test_search_by_content_existing_folder():
     assert set([f for f in files_found]) == expected_files
 
 
-def test_search_by_content_unexisting_folder():
-    files_scanner = source.search.Search()
+def test_search_by_content_unexisting_folder(word,files_scanner):
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "unexisted_folder")
-    word = "hello"
     with pytest.raises(source.exceptions.FolderNotFoundError):
         files_found = files_scanner.search_by_content(test_folder_path, word)
 
-def test_search_by_content_file_instead_of_folder():
-    files_scanner = source.search.Search()
+def test_search_by_content_file_instead_of_folder(word,files_scanner):
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_folder","test_file_hello")
-    word = "hello"
     with pytest.raises(NotADirectoryError):
         files_found = files_scanner.search_by_content(test_folder_path, word)
 
