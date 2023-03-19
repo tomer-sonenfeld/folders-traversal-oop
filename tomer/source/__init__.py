c3f4cd7502e996bb81e5b7cfabcaeec473c98525
tomerson <tomerson@bgu.ac.il>
2023-03-19 15:25:09 +0200

    init file applied

---
+++ tomer/source/__init__.py
@@ -0,0 +1,4 @@
+from directory import Directory
+from file import File
+from exceptions import FolderNotFoundError
+from search import Search
---
+++ tomer/tests/test_directory.py
@@ -0,0 +1,4 @@
+import tomer.source
+
+def test_traverse():
+    files_found = tomer.source.
