e5e5dbbed868d6b26c2f9d51f68481b36521c436
tomerson <tomerson@bgu.ac.il>
2023-03-19 16:38:39 +0200

    changed search_by_content to return string

--- tomer/source/search.py
+++ tomer/source/search.py
@@ -8,6 +8,6 @@ class Search:
     def search_by_content(self, path:str, word:str) -> list:
         if os.path.exists(path):
             _dir = Directory(path)
-            return _dir.files_with_content(word)
+            return set([f.path for f in _dir.files_with_content(word)])
         else:
             raise FolderNotFoundError(path)
