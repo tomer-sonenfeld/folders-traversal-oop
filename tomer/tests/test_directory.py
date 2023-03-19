1e5e409cb78376884f4f092beadfb857fd168655
tomerson <tomerson@bgu.ac.il>
2023-03-19 19:46:38 +0200

    edited tests to include addition of unicode error file

--- tomer/tests/test_directory.py
+++ tomer/tests/test_directory.py
@@ -5,11 +5,12 @@ def test_traverse():
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_folder")
     _dir = source.Directory(test_folder_path)
     files_found = _dir.traverse()
-    assert len(files_found) == 4
+    assert len(files_found) == 5
     expected_files = {os.path.join(test_folder_path,"test_file_hello"),
                       os.path.join(test_folder_path,"test_file_no_hello"),
                       os.path.join(test_folder_path,"test_sub_folder","test_file_hello"),
-                      os.path.join(test_folder_path,"test_sub_folder","test_file_no_hello")
+                      os.path.join(test_folder_path,"test_sub_folder","test_file_no_hello"),
+                      os.path.join(test_folder_path, "test_file_unicode_error")
                       }
     assert set([f.path for f in files_found]) == expected_files
 
--- tomer/tests/test_search.py
+++ tomer/tests/test_search.py
@@ -7,7 +7,9 @@ def test_search_by_content_existing_folder():
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_folder")
     word="hello"
     files_found = files_scanner.search_by_content(test_folder_path,word)
-    expected_files = {os.path.join(test_folder_path, "test_file_hello")}
+    expected_files = {os.path.join(test_folder_path, "test_file_hello"),
+                      os.path.join(test_folder_path,"test_sub_folder","test_file_hello")
+                     }
     assert set([f for f in files_found]) == expected_files
 
 
