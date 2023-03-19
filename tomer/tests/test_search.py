7ef2aead97061b955bdc38f742fccf8efd44dcd7
tomerson <tomerson@bgu.ac.il>
2023-03-19 19:01:56 +0200

    working tests for search

--- tomer/tests/test_search.py
+++ tomer/tests/test_search.py
@@ -1,10 +1,29 @@
 import os
 from tomer import source
+import pytest
 
-def test_search_by_content():
+def test_search_by_content_existing_folder():
     files_scanner = source.search.Search()
     test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_folder")
     word="hello"
     files_found = files_scanner.search_by_content(test_folder_path,word)
     expected_files = {os.path.join(test_folder_path, "test_file_hello")}
     assert set([f for f in files_found]) == expected_files
+
+
+def test_search_by_content_unexisting_folder():
+    files_scanner = source.search.Search()
+    test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "unexisted_folder")
+    word = "hello"
+    with pytest.raises(source.exceptions.FolderNotFoundError):
+        files_found = files_scanner.search_by_content(test_folder_path, word)
+
+def test_search_by_content_file_instead_of_folder():
+    files_scanner = source.search.Search()
+    test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_file")
+    word = "hello"
+    with pytest.raises(NotADirectoryError):
+        files_found = files_scanner.search_by_content(test_folder_path, word)
+
+
+
