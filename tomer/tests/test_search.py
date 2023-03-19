7b85e7db1d0fa4ca504a4971b87288d75395515f
tomerson <tomerson@bgu.ac.il>
2023-03-20 01:22:04 +0200

    renaming

--- tomer/tests/test_search.py
+++ tomer/tests/test_search.py
@@ -11,23 +11,23 @@ def files_scanner():
     return source.search.Search()
 
 def test_search_by_content_existing_folder(word,files_scanner):
-    test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder")
-    files_found = files_scanner.search_by_content(test_folder_path,word)
-    expected_files = {os.path.join(test_folder_path, "testing_file_include_word"),
-                      os.path.join(test_folder_path,"testing_sub_folder","testing_file_include_word")
+    testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder")
+    files_found = files_scanner.search_by_content(testing_folder_path,word)
+    expected_files = {os.path.join(testing_folder_path, "testing_file_include_word"),
+                      os.path.join(testing_folder_path,"testing_sub_folder","testing_file_include_word")
                      }
-    assert set([f for f in files_found]) == expected_files
+    assert set([_file for _file in files_found]) == expected_files
 
 
 def test_search_by_content_unexisting_folder(word,files_scanner):
-    test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "unexisted_folder")
+    testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "unexisted_folder")
     with pytest.raises(source.exceptions.FolderNotFoundError):
-        files_found = files_scanner.search_by_content(test_folder_path, word)
+        files_found = files_scanner.search_by_content(testing_folder_path, word)
 
 def test_search_by_content_file_instead_of_folder(word,files_scanner):
-    test_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_include_word")
+    testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_include_word")
     with pytest.raises(NotADirectoryError):
-        files_found = files_scanner.search_by_content(test_folder_path, word)
+        files_found = files_scanner.search_by_content(testing_folder_path, word)
 
 
 
