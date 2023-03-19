5ccf4519e86d351dc1796c31fa0bdde5830f3f67
tomerson <tomerson@bgu.ac.il>
2023-03-19 16:04:43 +0200

    changed list to set in files_found

--- tomer/source/directory.py
+++ tomer/source/directory.py
@@ -1,31 +1,30 @@
 
-
 import os
-from file import File
+from .file import File
 
 class Directory:
     def __init__(self, path:str):
         self._path=path
 
     def traverse(self) -> list:
-        files_found=[]
+        files_found=set()
         for inner_path in os.listdir(self._path):
             full_path=os.path.join(self._path,inner_path)
             if os.path.isfile(full_path):
                 _file = File(full_path)
-                files_found.append(_file)
+                files_found.add(_file)
             elif os.path.isdir(full_path):
                 sub_dir = Directory(full_path)
-                files_found.extend(sub_dir.traverse())
+                files_found.add(sub_dir.traverse())
         return files_found
 
 
     def files_with_content(self,word:str) -> list:
         files_found=self.traverse()
-        files_with_word=[]
+        files_with_word=set()
         for _file in files_found:
                 if _file.is_word_included(word):
-                    files_with_word.append(_file.path)
+                    files_with_word.add(_file.path)
         return files_with_word
 
 
