0e6f2ea950d75c897e2012878b841ef9cbfe84f7
tomerson <tomerson@bgu.ac.il>
2023-03-19 14:25:09 +0200

    removed main.py

--- tomer/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from search import Search
-
-if __name__ == "__main__":
-    files_scanner = Search()
-    result= files_scanner.search_by_content(os.getcwd(),"hello")
-    print(result)
