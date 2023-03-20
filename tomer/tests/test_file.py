7d178234e887c51e0f5da701b57b4ba67fbed8c3
tomerson <tomerson@bgu.ac.il>
2023-03-20 12:27:43 +0200

    removed prefix `is_word_included

--- tomer/tests/test_file.py
+++ tomer/tests/test_file.py
@@ -17,7 +17,7 @@ def test_is_word_included__word_not_included():
     _file = File(testing_file_path)
     assert not _file.is_word_included(word)
 
-def test_is_word_included_unreadable_file():
+def test_unreadable_file():
     word = "hello"
     testing_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_unicode_error")
     _file = File(testing_file_path)
