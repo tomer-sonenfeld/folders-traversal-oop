67581e5b33ab92dafc35baf842a878b7072f480a
tomerson <tomerson@bgu.ac.il>
2023-03-20 12:25:40 +0200

    double underscore on test function name

--- tomer/tests/test_file.py
+++ tomer/tests/test_file.py
@@ -5,13 +5,13 @@ import pytest
 from tomer.source.file import File
 
 
-def test_is_word_included_word_included():
+def test_is_word_included__word_included():
     word="hello"
     testing_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_include_word")
     _file = File(testing_file_path)
     assert _file.is_word_included(word)
 
-def test_is_word_included_word_not_included():
+def test_is_word_included__word_not_included():
     word = "hello"
     testing_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_exclude_word")
     _file = File(testing_file_path)
--- tomer/tests/test_search.py
+++ tomer/tests/test_search.py
@@ -8,7 +8,7 @@ import pytest
 def files_scanner():
     return Search()
 
-def test_search_by_content_existing_folder(files_scanner):
+def test_search_by_content__existing_folder(files_scanner):
     word="hello"
     testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder")
     files_found = files_scanner.search_by_content(testing_folder_path,word)
@@ -18,13 +18,13 @@ def test_search_by_content_existing_folder(files_scanner):
     assert set([_file for _file in files_found]) == expected_files
 
 
-def test_search_by_content_unexisting_folder(files_scanner):
+def test_search_by_content__unexisting_folder(files_scanner):
     word = "hello"
     testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "unexisted_folder")
     with pytest.raises(FolderNotFoundError):
         files_found = files_scanner.search_by_content(testing_folder_path, word)
 
-def test_search_by_content_file_instead_of_folder(files_scanner):
+def test_search_by_content__file_instead_of_folder(files_scanner):
     word = "hello"
     testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_include_word")
     with pytest.raises(NotADirectoryError):
