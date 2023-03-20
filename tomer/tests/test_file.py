dea86216e7612bb006719b66d9b3f2d739cd660f
tomerson <tomerson@bgu.ac.il>
2023-03-20 12:19:04 +0200

    removed hello fixture

--- tomer/tests/test_file.py
+++ tomer/tests/test_file.py
@@ -4,21 +4,21 @@ import os
 import pytest
 from tomer.source.file import File
 
-@pytest.fixture
-def word():
-    return "hello"
 
-def test_is_word_included_word_included(word):
+def test_is_word_included_word_included():
+    word="hello"
     testing_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_include_word")
     _file = File(testing_file_path)
     assert _file.is_word_included(word)
 
-def test_is_word_included_word_not_included(word):
+def test_is_word_included_word_not_included():
+    word = "hello"
     testing_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_exclude_word")
     _file = File(testing_file_path)
     assert not _file.is_word_included(word)
 
-def test_is_word_included_unreadable_file(word):
+def test_is_word_included_unreadable_file():
+    word = "hello"
     testing_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_unicode_error")
     _file = File(testing_file_path)
     with pytest.raises(UnicodeDecodeError):
