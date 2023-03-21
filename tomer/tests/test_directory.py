1cf08b0a7b0dd2fb4b9f04103fc806933a9f0c03
tomerson <tomerson@bgu.ac.il>
2023-03-21 23:45:18 +0200

    mocked all functions

--- tomer/tests/test_directory.py
+++ tomer/tests/test_directory.py
@@ -4,6 +4,7 @@ import os
 import pytest
 from mockito import mock, when, patch
 from tomer.source.directory import Directory
+from tomer.source.directory import File
 
 def test_traverse(paths,mock_isdir,mock_isFile,mock_listdir):
 
@@ -14,17 +15,27 @@ def test_traverse(paths,mock_isdir,mock_isFile,mock_listdir):
                       paths["testing_folder__testing_sub_folder__testing_file_include_word"]}
 
     _dir = Directory(paths["testing_folder"])
-    with patch('os.listdir', mock_listdir):
-        with patch('os.path.isfile', mock_isFile):
-            with patch('os.path.isdir', mock_isdir):
+    with patch('os.listdir', mock_listdir), patch('os.path.isfile', mock_isFile),\
+    patch('os.path.isdir', mock_isdir):
                 files_found = _dir.traverse()
     assert set([_file.path for _file in files_found]) == expected_files
 
 
-def test_files_with_content(testing_folder_path):
+def test_files_with_content(paths):
+
     word = "hello"
-    _dir = Directory(testing_folder_path)
+    _dir = Directory(paths["testing_folder"])
+
+    testing_file_exclude_word = File(paths["testing_folder__testing_file_exclude_word"])
+    testing_file_include_word = File(paths["testing_folder__testing_file_include_word"])
+
+    when(_dir).traverse().thenReturn([testing_file_include_word, testing_file_exclude_word])
+
+    when(testing_file_exclude_word).is_word_included(word).thenReturn(False)
+    when(testing_file_include_word).is_word_included(word).thenReturn(True)
+
+    expected_files = {paths["testing_folder__testing_file_include_word"]}
+
     files_found_with_word = _dir.files_with_content(word)
-    expected_files = {os.path.join(testing_folder_path, "testing_file_include_word"),
-                      os.path.join(testing_folder_path, "testing_sub_folder", "testing_file_include_word")}
+
     assert set([_file.path for _file in files_found_with_word]) == expected_files
