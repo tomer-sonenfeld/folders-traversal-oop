d3d06f0e0a92dae46d642db76545f42de4ff4408
tomerson <tomerson@bgu.ac.il>
2023-03-21 22:24:40 +0200

    split fixtures

--- tomer/tests/conftest.py
+++ tomer/tests/conftest.py
@@ -5,9 +5,8 @@ import os
 from mockito import when, mock,patch
 
 @pytest.fixture
-def testing_params() -> dict:
+def paths() -> dict:
 
-    params=dict()
     paths = dict()
     paths["testing_folder"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder")
     paths["testing_folder__testing_file_exclude_word"] = \
@@ -23,10 +22,11 @@ def testing_params() -> dict:
     paths["testing_folder__testing_sub_folder__testing_file_include_word"] = \
         os.path.join(paths["testing_folder__testing_sub_folder"], "testing_file_include_word")
 
-    mock_listdir = mock()
-    mock_isFile = mock()
-    mock_isdir = mock()
+    return paths
 
+@pytest.fixture()
+def mock_listdir(paths):
+    mock_listdir = mock()
 
     when(mock_listdir).__call__(paths["testing_folder"]).thenReturn(["testing_file_include_word",
                                                                  "testing_file_exclude_word",
@@ -39,6 +39,13 @@ def testing_params() -> dict:
                                                                  "testing_file_exclude_word"
                                                                  ])
 
+
+    return mock_listdir
+
+@pytest.fixture()
+def mock_isFile(paths):
+    mock_isFile = mock()
+
     when(mock_isFile).__call__(paths["testing_folder"]).thenReturn(False)
     when(mock_isFile).__call__(paths["testing_folder__testing_sub_folder"]).thenReturn(False)
     when(mock_isFile).__call__(paths["testing_folder__testing_file_exclude_word"]).thenReturn(True)
@@ -46,6 +53,12 @@ def testing_params() -> dict:
     when(mock_isFile).__call__(paths["testing_folder__testing_file_unicode_error"]).thenReturn(True)
     when(mock_isFile).__call__(paths["testing_folder__testing_sub_folder__testing_file_exclude_word"]).thenReturn(True)
     when(mock_isFile).__call__(paths["testing_folder__testing_sub_folder__testing_file_include_word"]).thenReturn(True)
+
+    return mock_isFile
+
+@pytest.fixture()
+def mock_isdir(paths):
+    mock_isdir = mock()
 
     when(mock_isdir).__call__(paths["testing_folder"]).thenReturn(True)
     when(mock_isdir).__call__(paths["testing_folder__testing_sub_folder"]).thenReturn(True)
@@ -55,9 +68,4 @@ def testing_params() -> dict:
     when(mock_isdir).__call__(paths["testing_folder__testing_sub_folder__testing_file_exclude_word"]).thenReturn(False)
     when(mock_isdir).__call__(paths["testing_folder__testing_sub_folder__testing_file_include_word"]).thenReturn(False)
 
-    params["paths"]=paths
-    params["mock_listdir"] = mock_listdir
-    params["mock_isFile"] = mock_isFile
-    params["mock_isdir"] = mock_isdir
-
-    return params
+    return mock_isdir
