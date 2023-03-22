833233382a78f5b6e7109dab0a7f8efb03d40ddc
shirdaniel <117354189+shirdaniel@users.noreply.github.com>
2023-03-22 15:12:12 +0200

    task1- Shir

--- directory.py
+++ directory.py
@@ -3,6 +3,10 @@ import directory
 from file import File
 
 
+class NotADirectoryErr(Exception):
+    pass
+
+
 class Directory:
 
     def __init__(self, name: str, parent_path: str = ""):
@@ -10,29 +14,26 @@ class Directory:
         self._parent_path = parent_path
         self._full_path = os.path.join(parent_path, name)
 
-    def list_dirs(self, word: str, files_list: list):
-        my_path = self._full_path
-        if os.path.isdir(my_path):
-            for name in os.listdir(my_path):
-                tmp_path = os.path.join(my_path, name)
-                if os.path.isdir(tmp_path):
-                    new_dir = Directory(name, my_path)
-                    new_dir.list_dirs(word, files_list)
-                elif os.path.isfile(tmp_path):
-                    self.check_word_in_file(tmp_path, word, files_list)
-        elif os.path.isfile(my_path):
-            self.check_word_in_file(my_path, word, files_list)
+        if not os.path.isdir(self._full_path):
+            raise NotADirectoryErr()
 
-    def check_word_in_file(self, my_path: str, word: str, files_list: list):
+    def list_dirs(self, word: str):
+        files_list = []
+        my_path = self._full_path
+        for name in os.listdir(my_path):
+            tmp_path = os.path.join(my_path, name)
+            if os.path.isdir(tmp_path):
+                new_dir = Directory(name, my_path)
+                files_list.extend(new_dir.list_dirs(word))
+            elif os.path.isfile(tmp_path):
+                self._check_word_in_file(tmp_path, word, files_list)
+        return files_list
+
+    def _check_word_in_file(self, my_path: str, word: str, files_list: list):
         new_file = File(my_path)
-        if new_file._find_word(word):
+        if new_file.find_word(word):
             files_list.append(my_path)
 
-    if __name__ == '__main__' :
+    if __name__ == '__main__':
         dir = directory.Directory("D:\PycharmProjects\pypy")
-        output_files = []
-        dir.list_dirs('shir', output_files)
-        print(output_files)
-
-
-
+        print(dir.list_dirs('shir'))
--- file.py
+++ file.py
@@ -1,10 +1,12 @@
 import os
+
+
 class File:
 
     def __init__(self, full_path: str):
         self._full_path = full_path
 
-    def _find_word(self, word: str):
+    def find_word(self, word: str):
         try:
             with open(self._full_path) as file:
                 if word in file.read():
