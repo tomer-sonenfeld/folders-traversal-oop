88c50f0d38c4c2792a4ecae9e9569cc6feaa3396
shirdaniel <117354189+shirdaniel@users.noreply.github.com>
2023-03-22 13:09:26 +0200

    tast1 - Shir

--- directory.py
+++ directory.py
@@ -1,8 +1,11 @@
 import os
+import directory
 from file import File
 
+
 class Directory:
-    def __init__(self, name: str, parent_path: str):
+
+    def __init__(self, name: str, parent_path: str = ""):
         self._name = name
         self._parent_path = parent_path
         self._full_path = os.path.join(parent_path, name)
@@ -22,7 +25,14 @@ class Directory:
 
     def check_word_in_file(self, my_path: str, word: str, files_list: list):
         new_file = File(my_path)
-        if new_file.find_word(word):
+        if new_file._find_word(word):
             files_list.append(my_path)
+
+    if __name__ == '__main__' :
+        dir = directory.Directory("D:\PycharmProjects\pypy")
+        output_files = []
+        dir.list_dirs('shir', output_files)
+        print(output_files)
+
 
 
