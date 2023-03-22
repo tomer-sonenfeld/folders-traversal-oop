dfe6ab2df0f189883a17d77f22e624c0fd8db8bd
Saar <saarb97@gmail.com>
2023-03-22 08:35:11 +0200

    based on CR, scanning folders is now dynamic. Additional fixes of issues.

--- directory.py
+++ directory.py
@@ -10,11 +10,10 @@ class FailedToReadFromFile(Exception):
 
 
 class Directory(object):
-    def __init__(self, path: str, father_directory: 'Directory' = None):
+    def __init__(self, path: str):
         if not os.path.isdir(path):
             raise FailedToCreateDirectory("path is not a directory")
         self._path = path
-        self._father_directory = father_directory
         self._subdirectories = []
         self._files = []
 
@@ -22,49 +21,37 @@ class Directory(object):
     def path(self):
         return self._path
 
-    @property
-    def father_directory(self):
-        return self._father_directory
-
-    @property
-    def subdirectories(self):
-        return self._subdirectories
-
-    @property
-    def files(self):
-        return self._files
-
-    def scan_folder(self):
-        for item in os.listdir(self._path):
-            item_path = os.path.join(self._path, item, "")
-            if os.path.isdir(os.path.join(self._path, item)):
-                _dir = next((existing_subdir for existing_subdir in self._subdirectories
-                             if existing_subdir.path == item_path), None)
-                if _dir is None:
+    def _scan_folder(self):
+        for relative_path in os.listdir(self._path):
+            full_path = os.path.join(self._path, relative_path)
+            if os.path.isdir(full_path):
+                sub_dir_exists = False
+                for sub_dir in self._subdirectories:
+                    if sub_dir.path == full_path:
+                        sub_dir_exists = True
+                        break
+                if not sub_dir_exists:
                     try:
-                        x = Directory(item_path, self)
-                        self._subdirectories.append(x)
-                    except:
-                        pass
-                else:
-                    _dir.scan_folder()
-            elif item_path not in self._files:
-                self._files.append(os.path.join(self._path, item))
+                        new_subdir = Directory(full_path)
+                        self._subdirectories.append(new_subdir)
+                    except FailedToCreateDirectory:
+                        print(f'Failed to create directory: {full_path}')
+            elif full_path not in self._files:
+                self._files.append(full_path)
 
     def lookup_str_in_files(self, word: str):
         if not os.path.isdir(self._path):
-            raise FailedToCreateDirectory("path is no longer a directory")
-        self.scan_folder()
+            print(f'Directory {self._path} no longer exists.')  # Should I print the error?
+            return
+        self._scan_folder()
         res = []
         for _file in self._files:
             try:
-                f = open(_file, "r")
-                if word in f:
-                    res.append(_file)
-            except:
-                pass
-            finally:
-                f.close()
+                with open(_file) as file_object:
+                    if word in file_object.read():
+                        res.append(_file)
+            except OSError:
+                print(f"Failed to open file: {_file}")
         for _dir in self._subdirectories:
             res.extend(_dir.lookup_str_in_files(word))
         return res
