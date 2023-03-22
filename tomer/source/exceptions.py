2bc684703b4ff84cfd8217cc725e8d22590b4842
tomerson <tomerson@bgu.ac.il>
2023-03-22 16:59:51 +0200

    left 1 exception

--- tomer/source/exceptions.py
+++ tomer/source/exceptions.py
@@ -1,8 +1,7 @@
 
 
-class FolderNotFoundError(Exception):
-    def __init__(self, folder_path:str):
-        message = f"Folder not found: {folder_path}"
-        super().__init__(message)
+class UnexistedFolder(Exception):
+    pass
+
 
 
