579b79144ac2f95cc29a6760b241efb8de70e533
tomerson <tomerson@bgu.ac.il>
2023-03-23 20:02:28 +0200

    tests for search

--- tomer/tests/test_search.py
+++ tomer/tests/test_search.py
@@ -1,6 +1,9 @@
 import os
 from tomer.source.search import Search
-from tomer.source.exceptions import FolderNotFoundError
+from tomer.source.exceptions import UnexistedFolder
+from tomer.source.directory import Directory
+from tomer.source.my_class import original_function
+from mockito import when, patch, mock
 import pytest
 
 
@@ -8,24 +11,34 @@ import pytest
 def files_scanner():
     return Search()
 
-def test_search_by_content__existing_folder(files_scanner):
+def test_search_by_content__unexisted_path(paths,files_scanner):
     word="hello"
-    testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder")
+    unexisted_path = paths["testing_folder\\unexisted_path"]
+    when(os.path).exists(unexisted_path).thenReturn(False)
+    with pytest.raises(UnexistedFolder):
+        files_scanner.search_by_content(unexisted_path,word)
+
+def test_search_by_content__path_is_file(paths,files_scanner):
+    word="hello"
+    testing_file_path = paths["testing_folder\\testing_file"]
+    when(os.path).exists(testing_file_path).thenReturn(True)
+    when(os.path).isdir(testing_file_path).thenReturn(False)
+    expected_files=testing_file_path
+    files_found=files_scanner.search_by_content(testing_file_path,word)
+    assert files_found == expected_files
+
+
+def test_search_by_content__existing_folder(paths,files_scanner):
+    word = "hello"
+    testing_folder_path = paths["testing_folder"]
+    when(os.path).exists(testing_folder_path).thenReturn(True)
+    when(os.path).isdir(testing_folder_path).thenReturn(True)
+    _dir = Directory(testing_folder_path)
+    when(Directory).files_with_content(word).thenReturn(paths["testing_folder\\testing_file_include_word"])
     files_found = files_scanner.search_by_content(testing_folder_path,word)
-    expected_files = {os.path.join(testing_folder_path, "testing_file_include_word"),
-                      os.path.join(testing_folder_path,"testing_sub_folder","testing_file_include_word")
-                     }
-    assert set([_file for _file in files_found]) == expected_files
+    expected_files = paths["testing_folder\\testing_file_include_word"]
+    assert files_found == expected_files
 
 
-def test_search_by_content__unexisting_folder(files_scanner):
-    word = "hello"
-    testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "unexisted_folder")
-    with pytest.raises(FolderNotFoundError):
-        files_found = files_scanner.search_by_content(testing_folder_path, word)
 
-def test_search_by_content__file_instead_of_folder(files_scanner):
-    word = "hello"
-    testing_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing_folder", "testing_file_include_word")
-    with pytest.raises(NotADirectoryError):
-        files_found = files_scanner.search_by_content(testing_folder_path, word)
+
