5ac12ba38e2bda71345299482193f6eafd7ca7c1
tomerson <tomerson@bgu.ac.il>
2023-03-26 10:19:47 +0300

    testing order

--- tomer/tests/conftest.py
+++ tomer/tests/conftest.py
@@ -7,8 +7,9 @@ from mockito import when, mock
 
 @pytest.fixture
 def mocked_open(request):
+    testing_file_path = request.param
     file_mock = mock()
-    when(builtins).open(request.param, 'r').thenReturn(file_mock)
+    when(builtins).open(testing_file_path, 'r').thenReturn(file_mock)
     when(file_mock).__enter__().thenReturn(file_mock)
     when(file_mock).__exit__(None, None, None).thenReturn()
     return file_mock
