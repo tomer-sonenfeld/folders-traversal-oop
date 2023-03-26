22d0c592b1887f886a802063c5d8637172c396d3
tomerson <tomerson@bgu.ac.il>
2023-03-26 05:39:50 +0300

    arranged tests

--- tomer/tests/test_directory.py
+++ tomer/tests/test_directory.py
@@ -1,17 +1,18 @@
 
 
 import os
-from mockito import when, verifyStubbedInvocationsAreUsed, unstub
+from mockito import when, verifyStubbedInvocationsAreUsed, unstub, verify
 from tomer.source.directory import Directory
 from tomer.source.directory import File
 
 
 def test_traverse__file_found():
-    expected = {os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1")}
-    when(os).listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir")).thenReturn(["file1"])
-    when(os.path).isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1")).thenReturn(True)
+    expected = {'/root_dir/file1'}
+    when(os).listdir('/root_dir').thenReturn(["file1"])
+    when(os.path).join('/root_dir','file1').thenReturn('/root_dir/file1')
+    when(os.path).isfile('/root_dir/file1').thenReturn(True)
 
-    tested = Directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir"))
+    tested = Directory('/root_dir')
     result = tested.traverse()
 
     assert set([_file.path for _file in result]) == expected
@@ -21,16 +22,18 @@ def test_traverse__file_found():
 
 
 def test_traverse__nested_files_found():
-    expected = {os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"),
-                os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir", "file2")}
-    when(os).listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir")).thenReturn(["file1", "sub_dir"])
-    when(os.path).isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1")).thenReturn(True)
-    when(os.path).isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir")).thenReturn(False)
-    when(os.path).isdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir")).thenReturn(True)
-    when(os).listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir")).thenReturn(["file2"])
-    when(os.path).isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir", "file2")).thenReturn(True)
+    expected = {'/root_dir/file1', '/root_dir/sub_dir/file2'}
+    when(os).listdir('/root_dir').thenReturn(["file1", "sub_dir"])
+    when(os.path).join('/root_dir','file1').thenReturn('/root_dir/file1')
+    when(os.path).isfile('/root_dir/file1').thenReturn(True)
+    when(os.path).join('/root_dir','sub_dir').thenReturn('/root_dir/sub_dir')
+    when(os.path).isdir('/root_dir/sub_dir').thenReturn(True)
+    when(os.path).isfile('/root_dir/sub_dir').thenReturn(False)
+    when(os).listdir('/root_dir/sub_dir').thenReturn(["file2"])
+    when(os.path).join('/root_dir/sub_dir','file2').thenReturn('/root_dir/sub_dir/file2')
+    when(os.path).isfile('/root_dir/sub_dir/file2').thenReturn(True)
 
-    tested = Directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir"))
+    tested = Directory('/root_dir')
     result = tested.traverse()
 
     assert set([_file.path for _file in result]) == expected
@@ -40,10 +43,10 @@ def test_traverse__nested_files_found():
 
 
 def test_files_with_content__file_with_word():
-    expected = {os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1")}
+    expected = {'/root_dir/file1'}
     word = "hello"
-    tested_folder = Directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
-    tested_file = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
+    tested_folder = Directory('/root_dir')
+    tested_file = File('/root_dir/file1')
     when(tested_folder).traverse().thenReturn([tested_file])
     when(tested_file).is_word_included(word).thenReturn(True)
 
@@ -58,8 +61,8 @@ def test_files_with_content__file_with_word():
 def test_files_with_content__file_without_word():
     expected = set()
     word = "hello"
-    tested_folder = Directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
-    tested_file = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
+    tested_folder = Directory('/root_dir')
+    tested_file = File('/root_dir/file1')
     when(tested_folder).traverse().thenReturn([tested_file])
     when(tested_file).is_word_included(word).thenReturn(False)
 
@@ -72,11 +75,11 @@ def test_files_with_content__file_without_word():
 
 
 def test_files_with_content__file_with_word_and_without_word():
-    expected = {os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1")}
+    expected = {'/root_dir/file1'}
     word = "hello"
-    tested_folder = Directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
-    tested_file_include_word = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
-    tested_file_exclude_word = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file2"))
+    tested_folder = Directory('/root_dir')
+    tested_file_include_word = File('/root_dir/file1')
+    tested_file_exclude_word = File('/root_dir/file2')
     when(tested_folder).traverse().thenReturn([tested_file_include_word,tested_file_exclude_word])
     when(tested_file_include_word).is_word_included(word).thenReturn(True)
     when(tested_file_exclude_word).is_word_included(word).thenReturn(False)
@@ -90,13 +93,12 @@ def test_files_with_content__file_with_word_and_without_word():
 
 
 def test_files_with_content__file_with_word_and_without_word_and_nested_file_with_word():
-    expected = {os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"),
-                os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir", "file2")}
+    expected = {'/root_dir/file1', '/root_dir/sub_dir/file2'}
     word = "hello"
-    tested_folder = Directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
-    tested_file_include_word = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file1"))
-    tested_file_exclude_word = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "file2"))
-    tested_file_nested_include_word = File(os.path.join(os.path.dirname(os.path.abspath(__file__)), "root_dir", "sub_dir", "file2"))
+    tested_folder = Directory('/root_dir')
+    tested_file_include_word = File('/root_dir/file1')
+    tested_file_exclude_word = File('/root_dir/file2')
+    tested_file_nested_include_word = File('/root_dir/sub_dir/file2')
     when(tested_folder).traverse().thenReturn([tested_file_include_word,tested_file_exclude_word,tested_file_nested_include_word])
     when(tested_file_include_word).is_word_included(word).thenReturn(True)
     when(tested_file_exclude_word).is_word_included(word).thenReturn(False)
