ea8fd5575b831de65eaa09abe60aa748a1da8e8d
tomerson <tomerson@bgu.ac.il>
2023-03-27 10:57:38 +0300

    conftest teardown option

---
+++ tomer/tests/conftest.py
@@ -0,0 +1,12 @@
+
+
+import pytest
+from mockito import unstub, verifyStubbedInvocationsAreUsed
+
+@pytest.fixture
+def teardown():
+    yield
+    try:
+        verifyStubbedInvocationsAreUsed()
+    finally:
+        unstub()
