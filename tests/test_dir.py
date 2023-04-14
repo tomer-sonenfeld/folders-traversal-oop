1e793fbd6af5eb21f9b3f233d2aa4f045cf70e4c
nirrose <nirrose@post.bgu.ac.il>
2023-04-14 23:44:09 +0300

    tests for word match function in file class

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -3,62 +3,10 @@ import pytest
 from training.dir import Dir
 import os.path
 from mockito import when, mock, unstub
-import training.file_path as file_path
+import training.file as file_path
 
 
-def test_path_property_no_parent():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).isdir('root').thenReturn(True)
-    fullpath = 'root'
-    curr_dir = Dir(fullpath)
-    assert fullpath == curr_dir.path
-
-
-def test_path_property_with_parent():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).isdir('root').thenReturn(True)
-    fullpath = 'root'
-    parent_dir = Dir(fullpath)
-    when(os.path).join("root", "fold0").thenReturn('root\\fold0')
-    when(os.path).isdir('root\\fold0').thenReturn(True)
-    child_dir = Dir("fold0", parent_dir)
-    assert "fold0" == child_dir.path
-
-
-def test_fullpath_property_no_parent():
-    when(os.path).join("", "root").thenReturn('root')
-    fullpath = 'root'
-    curr_dir = Dir(fullpath)
-    assert fullpath == curr_dir.fullpath
-
-
-def test_fullpath_property_with_parent():
-    when(os.path).join("", "root").thenReturn('root')
-    fullpath = 'root'
-    parent_dir = Dir(fullpath)
-    when(os.path).join("root", "fold0").thenReturn('root\\fold0')
-    child_dir = Dir("fold0", parent_dir)
-    assert 'root\\fold0' == child_dir.fullpath
-
-
-def test_parent_property_no_parent():
-    when(os.path).join("", "root").thenReturn('root')
-    fullpath = 'root'
-    curr_dir = Dir(fullpath)
-    assert curr_dir.parent is None
-
-
-def test_parent_property_with_parent():
-    when(os.path).join("", "root").thenReturn('root')
-    fullpath = 'root'
-    parent_dir = Dir(fullpath)
-    when(os.path).join("root", "fold0").thenReturn('root\\fold0')
-    child_dir = Dir("fold0", parent_dir)
-    assert "fold0" == child_dir.path
-
-
-def test_match_files_os_listdir_empty():
-    when(os.path).join("", "root").thenReturn('root')
+def test_match_files__os_listdir_is_empty__no_fail():
     when(os.path).isdir('root').thenReturn(True)
     when(os).listdir('root').thenReturn([])
 
@@ -68,100 +16,85 @@ def test_match_files_os_listdir_empty():
     assert set(result) == set()
 
 
-def test_match_files_os_listdir_not_empty():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).join('root', "file0").thenReturn("root\\file0")
+def test_match_files__os_listdir_not_empty__no_fail():
     when(os.path).isdir('root').thenReturn(True)
-    when(os.path).isdir("root\\file0").thenReturn(False)
+    when(os.path).isdir("root/file0").thenReturn(False)
     when(os).listdir('root').thenReturn(["file0"])
     fullpath = 'root'
     root_dir = Dir(fullpath)
     result = root_dir.match_files("list_dir_not_empty")
     assert set(result) == set()
 
-def test_match_files_file_path_from_path_none():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).join('root', "file0").thenReturn("root\\file0")
+def test_match_files__file_path_from_path_returns_none__no_fail():
     when(os.path).isdir('root').thenReturn(True)
-    when(os.path).isdir("root\\file0").thenReturn(False)
+    when(os.path).isdir("root/file0").thenReturn(False)
     when(os).listdir('root').thenReturn(["file0"])
-    when(file_path.File).from_path("root\\file0").thenReturn(None)
+    when(file_path.File).from_path("root/file0").thenReturn(None)
     fullpath = 'root'
     root_dir = Dir(fullpath)
     result = root_dir.match_files("list_dir_not_empty")
     assert set(result) == set()
 
-def test_match_files_file_path_from_path_not_none():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).join('root', "file0").thenReturn("root\\file0")
+def test_match_files__file_path_from_path_not_none__no_fail():
     when(os.path).isdir('root').thenReturn(True)
-    when(os.path).isdir("root\\file0").thenReturn(False)
+    when(os.path).isdir("root/file0").thenReturn(False)
     when(os).listdir('root').thenReturn(["file0"])
     mock_file0 = mock()
-    when(file_path.File).from_path("root\\file0").thenReturn(mock_file0)
+    when(file_path.File).from_path("root/file0").thenReturn(mock_file0)
     when(mock_file0).word_match('in_directory').thenReturn(False)
     fullpath = 'root'
     root_dir = Dir(fullpath)
     result = root_dir.match_files("list_dir_not_empty")
     assert set(result) == set()
 
-def test_match_files_file_only_in_root_directory():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).join('root', "file0").thenReturn("root\\file0")
+def test_match_files__file_only_in_root_directory__one_match():
     when(os.path).isdir('root').thenReturn(True)
-    when(os.path).isdir("root\\file0").thenReturn(False)
+    when(os.path).isdir("root/file0").thenReturn(False)
 
     mock_file0 = mock()
-    when(file_path.File).from_path("root\\file0").thenReturn(mock_file0)
+    when(file_path.File).from_path("root/file0").thenReturn(mock_file0)
     when(mock_file0).word_match('in_directory').thenReturn(True)
     fullpath = 'root'
     root_dir = Dir(fullpath)
     result = root_dir.match_files("in_directory")
-    assert set(result) == {"root\\file0"}
+    assert set(result) == {"root/file0"}
 
 
-def test_match_files_file_in_sub_root_directory():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).join("root\\fold0", "file1").thenReturn("root\\fold0\\file1")
+def test_match_files__file_in_sub_root_directory__one_match():
     when(os.path).isdir('root').thenReturn(True)
-    when(os.path).isdir('root\\fold0').thenReturn(True)
-    when(os).listdir('root').thenReturn(["fold0"])
-    when(os).listdir('root\\fold0').thenReturn(["file1"])
+    when(os.path).isdir('root/fold0').thenReturn(True)
 
-    when(os.path).join('root\\fold0', "file1").thenReturn("root\\fold0\\file1")
-    when(os.path).isdir("root\\fold0\\file1").thenReturn(False)
-    when(os).listdir('root\\fold0').thenReturn(["file1"])
+    when(os).listdir('root').thenReturn(["fold0"])
+    when(os).listdir('root/fold0').thenReturn(["file1"])
+    when(os.path).isdir("root/fold0/file1").thenReturn(False)
+    when(os).listdir('root/fold0').thenReturn(["file1"])
 
     mock_file1 = mock()
-    when(file_path.File).from_path("root\\fold0\\file1").thenReturn(mock_file1)
+    when(file_path.File).from_path("root/fold0/file1").thenReturn(mock_file1)
     when(mock_file1).word_match('in_directory').thenReturn(True)
     fullpath = 'root'
     root_dir = Dir(fullpath)
     result = root_dir.match_files("in_directory")
-    assert set(result) == {"root\\fold0\\file1"}
+    assert set(result) == {"root/fold0/file1"}
 
 
-def test_match_files_2_files_in_directory_and_sub_root():
-    when(os.path).join("", "root").thenReturn('root')
-    when(os.path).join('root', "file0").thenReturn("root\\file0")
-    when(os.path).join("root\\fold0", "file1").thenReturn("root\\fold0\\file1")
-
+def test_match_files__two_files_in_directory_and_sub_root__two_matches():
     when(os.path).isdir('root').thenReturn(True)
-    when(os.path).isdir('root\\fold0').thenReturn(True)
-    when(os.path).isdir("root\\file0").thenReturn(False)
-    when(os.path).isdir("root\\fold0\\file1").thenReturn(False)
+    when(os.path).isdir('root/fold0').thenReturn(True)
+    when(os.path).isdir("root/file0").thenReturn(False)
+    when(os.path).isdir("root/fold0/file1").thenReturn(False)
 
     when(os).listdir('root').thenReturn(["fold0", "file0"])
-    when(os).listdir('root\\fold0').thenReturn(["file1"])
+    when(os).listdir('root/fold0').thenReturn(["file1"])
 
     mock_file = mock()
-    when(file_path.File).from_path("root\\file0").thenReturn(mock_file)
+    when(file_path.File).from_path("root/file0").thenReturn(mock_file)
     when(mock_file).word_match('in_directory').thenReturn(True)
 
-    when(file_path.File).from_path("root\\fold0\\file1").thenReturn(mock_file)
+    when(file_path.File).from_path("root/fold0/file1").thenReturn(mock_file)
     when(mock_file).word_match('in_directory').thenReturn(True)
 
     fullpath = 'root'
     root_dir = Dir(fullpath)
     result = root_dir.match_files("in_directory")
-    assert set(result) == {"root\\file0", "root\\fold0\\file1"}
+    assert set(result) == {"root/file0", "root/fold0/file1"}
--- tests/test_file.py
+++ tests/test_file.py
@@ -1,9 +1,52 @@
 import pytest
 from training.dir import Dir
-import os.path
+import builtins
 from mockito import when, mock
-import training.file_path as file_path
+from training.file import File
 
 
 # from path - file/not file
-# word_match - open fails, open success - word found, word not found
+def test_from_path_not_file_expected_null():
+    assert True
+
+def test_from_path_good_file_path_expected_file_object():
+    assert True
+
+
+def test_word_match_open_file_fails():
+    bad_fullpath = "root/bad_file"
+    test_file = File(bad_fullpath)
+    word = "test fail"
+    try:
+        test_file.word_match(word)
+    except:
+        pass
+    else:
+        assert False, "FileNotFoundError has not been outputed"
+
+
+def test_word_match_word_found_in_file():
+    filepath = "root/file0"
+    test_file = File(filepath)
+    mock_file = mock()
+    when(builtins).open(filepath, "r").thenReturn(mock_file)
+    when(mock_file).__enter__().thenReturn(mock_file)
+    when(mock_file).__exit__().thenReturn()
+    test_file_string = "string file representation has test keyword"
+    when(mock_file).read().thenReturn(test_file_string)
+    word = "test"
+    assert test_file.word_match(word)
+
+
+def test_word_match_word_not_found_in_file():
+    filepath = "root/file0"
+    test_file = File(filepath)
+    mock_file = mock()
+    when(builtins).open(filepath, "r").thenReturn(mock_file)
+    when(mock_file).__enter__().thenReturn(mock_file)
+    when(mock_file).__exit__().thenReturn()
+    test_file_string = "string file representation has test keyword"
+    when(mock_file).read().thenReturn(test_file_string)
+    word = "nothing"
+    assert not test_file.word_match(word)
+
